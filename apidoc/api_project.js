7c5ad6c025ac1319cf4faf80f41653757b19400b
Varun Gupta <varun-glory369@gmail.com>
2019-07-30 14:53:00 +0530

    link

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "APi Documentation",  "version": "0.0.1",  "description": "",  "title": "Apis",  "url": "http://localhost:3000",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-07-28T08:07:29.167Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "APi Documentation",
+  "version": "0.0.1",
+  "description": "",
+  "title": "Apis",
+  "url": "http://api2.testautotech.xyz",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-07-28T08:07:29.167Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
